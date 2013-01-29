d2ef290324d48261bee2a69d059c456d387e9563
Thang-Le <leducthang290193@yahoo.com>
2013-01-29 23:27:00 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,5 +5,6 @@ print("enter numders that you want to combine")
 so1 = input("first number")
 so2 = input("second number")
 cal = calculator.calculate(so1,so2)
-cal.display()
+calcifer = cal.display()
+mess.mss(calcifer)
 
