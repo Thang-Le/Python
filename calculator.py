4b3c9ee2d41602f1bb68e56ba9a153e32c1efb6c
Thang-Le <leducthang290193@yahoo.com>
2013-01-29 23:31:16 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,13 +5,16 @@ class calculate(object):
         self.number2 = number2
     def display(self):
         answer = str(input("enter Operation:"))
-        if(str(answer) == 'add'):
+        elif(str(answer) == 'add'):
             total = float(self.number1)+float(self.number2)
-        if(str(answer) == 'subtract'):
+        elif(str(answer) == 'subtract'):
             total = float(self.number1)-float(self.number2)
-        if(str(answer)== 'multiply'):
+        elif(str(answer)== 'multiply'):
             total = float(self.number1)*float(self.number2)
-        if(str(answer)== 'divide'):
-            total = float(self.number1)/float(self.number2)
+        elif(str(answer)== 'divide'):
+            if(self.number2 == 0):
+                print("WARNING")
+            else:
+                total = float(self.number1)/float(self.number2)
         else: print ("wrong ope")
         return total
